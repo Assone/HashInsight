9690e6d604bda57ea78cf7413e68b83657a83c6b
Assone <14324389+Assone@users.noreply.github.com>
2025-04-06 11:28:38 +0800

    feat: description add sha-384 support

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,10 @@ const App: React.FC = () => {
     <LazyMotion features={loadFeatures}>
       <div className="h-full flex flex-col items-center justify-center gap-4 p-4">
         <h1 className="text-4xl font-bold">HashInsight</h1>
-        <p>Compute the MD5, SHA-1, SHA-256, and SHA-512 hashes of the file.</p>
+        <p>
+          Compute the MD5, SHA-1, SHA-256, SHA-384, and SHA-512 hashes of the
+          file.
+        </p>
         <UploadFile onChange={setFile} />
         {file && <FileInfo file={file} />}
         {file && <InspectInfo className="w-4/5" file={file} />}
